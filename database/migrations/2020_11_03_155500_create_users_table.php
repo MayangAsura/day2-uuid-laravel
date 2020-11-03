3429945ce7360eaf0dbe3b5da305b34bc75cd45e
MayangAsura <mayang.asura123@gmail.com>
2020-11-03 23:11:37 +0700

    23:12

---
+++ database/migrations/2020_11_03_155500_create_users_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            // $table->foreignId('roles_id')->constrained('roles')->onDelete('cascade')->onUpdate('cascade');
+            // $table->foreignId('otp_codes_id')->constrained('otp_codes')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('roles_id');
+            $table->uuid('otp_codes_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('roles_id')->references('id')->on('roles')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('otp_codes_id')->references('id')->on('otp_codes')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
