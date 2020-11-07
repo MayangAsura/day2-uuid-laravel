428f67082ef95518472275f91cc36c6e5203de62
MayangAsura <mayang.asura123@gmail.com>
2020-11-07 22:16:49 +0700

    route for regenerate_otp

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::namespace('Auth')->group(function(){
     Route::post('register', 'RegisterController');
     Route::post('login', 'LoginController');
     Route::post('verification', 'VerificationController');
+    Route::post('regenerate_otp', 'RegenerateOtpController');
 
 });
 
